12b6895db9130ce9d780751ce9566a130ad0ec63
Yann Thomas-Gerard <inside@gmail.com>
2009-10-29 18:53:40 +0100

    WIP

--- junofetch.py
+++ junofetch.py
@@ -2,13 +2,21 @@
 
 import sys
 import re
+import urllib2
 
 def get_file(file):
-    try:
-        f = open(file, 'r')
-    except IOError, e:
-        print e
-        sys.exit(1)
+    if re.search('^http://', file):
+        try:
+            f = urllib2.urlopen(file)
+        except urllib2.URLError, e:
+            print e
+            sys.exit(1)
+    else:
+        try:
+            f = open(file, 'r')
+        except IOError, e:
+            print e
+            sys.exit(1)
 
     str = f.read()
     f.close()
@@ -18,11 +26,11 @@ def get_base_url():
     return 'http://www.junodownload.com/'
 
 # Main
-#if len(sys.argv) <= 1:
-#    print 'Usage: ' + sys.argv[0] + ' [juno mailing list link or html file]'
-#    sys.exit(1)
+if len(sys.argv) <= 1:
+    print 'Usage: ' + sys.argv[0] + ' [juno mailing list link or html file]'
+    sys.exit(1)
 
-match = re.search('href="([^"]+\.m3u)"', get_file('a.html'))
+match = re.search('href="([^"]+\.m3u)"', get_file(sys.argv[1]))
 
 if match == None:
     print 'Couldn\'t find m3u link'
@@ -30,4 +38,5 @@ if match == None:
 
 m3uUrl = get_base_url() + match.group(1)
 content = get_file(m3uUrl)
+
 print content
